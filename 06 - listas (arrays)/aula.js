e98512e82edc8dc4a47b20f609dfda68937cb0d3
Maria <mariafcknt@outlook.com>
2023-12-14 14:38:57 -0300

    Create aula.js

---
+++ 06 - listas (arrays)/aula.js
@@ -0,0 +1,19 @@
+/*const alunos = [1, 2, 3];
+
+Adicionar itens na lista
+alunos.push('Marta');
+alunos[4] = 'Vanessa';
+
+tira o último item da lista
+//console.log(alunos.pop());
+
+tira o primeiro item da lista
+console.log(alunos.shift());
+
+console.log(alunos)*/
+
+const notas = [1, 2, 3]
+
+for (let i = 0; i < notas.length; i ++) {
+    console.log(notas[i])
+}
