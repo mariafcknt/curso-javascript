29131bc0a9f0a55e301e8193f772fecd84e943ac
Maria <mariafcknt@outlook.com>
2023-12-14 15:17:29 -0300

    exercicio omaior

--- 07 - importacao/funcoes_auxiliares.js
+++ 07 - importacao/funcoes_auxiliares.js
@@ -1,9 +1,21 @@
-function gets() {
+/*function gets() {
     return 10;
 }
 
 function print(texto) {
     console.log(texto);
+}*/
+
+
+function maiorNumeroSorteado() {
+    var omaior = 0;
+    numeros = [5, 50, 10, 98, 23];
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] > omaior) {
+            omaior = numeros[i]
+        }
+    }
+    return omaior;
 }
 
-module.exports = {gets, print};
+module.exports = {maiorNumeroSorteado};
--- 07 - importacao/main.js
+++ 07 - importacao/main.js
@@ -1,6 +1,9 @@
-const {gets, print} = require('./funcoes_auxiliares');
+/*const {gets, print} = require('./funcoes_auxiliares');
 
-//console.log(funcoes.gets())
+console.log(funcoes.gets())
 
+print(gets());*/
 
-print(gets());
+const {maiorNumeroSorteado} = require('./funcoes_auxiliares');
+
+console.log(maiorNumeroSorteado())
