f4ccf021a9463714999763f1516f47a3f130f3ea
Maria <mariafcknt@outlook.com>
2023-12-19 13:05:28 -0300

    fix aula08

--- 08 - avaliacao/ex02_funcoes_auxiliares.js
+++ 08 - avaliacao/ex02_funcoes_auxiliares.js
@@ -1,8 +1,9 @@
 const entradas = [5, 3, 4, 1, 10, 8];
-let i = 0;
+
 
 function gets() {
     const valores = entradas;
+
     return valores;
 }
 
--- 08 - avaliacao/ex02_maior_menor.js
+++ 08 - avaliacao/ex02_maior_menor.js
@@ -1,14 +1,21 @@
 const {gets, print} = require('./ex02_funcoes_auxiliares.js')
 
-maior_par = 0;
-menor_impar = 9999;
+maior_par = null;
+menor_impar = null;
 const numeros = gets();
 
+
 for (let i = 0; i < numeros.length; i++) {
-    if (numeros[i] % 2 === 0 && numeros[i] > maior_par) {
-        maior_par = numeros[i];
-    } else if (numeros[i] % 2 === 1 && numeros[i] < menor_impar) {
-        menor_impar = numeros[i];
+    
+    if (numeros[i] % 2 === 0) {
+        if (maior_par == null || numeros[i] > maior_par) {
+            maior_par = numeros[i]; 
+        }
+    } 
+    else if (numeros[i] % 2 === 1) {
+        if (menor_impar == null || numeros[i] < menor_impar) {
+            menor_impar = numeros[i];
+        } 
     }
 }
 
