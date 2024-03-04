0c16828d51f079a41476e745bee96a513fe3d087
Maria <mariafcknt@outlook.com>
2024-03-04 11:45:29 -0300

    aula 09 e 10

---
+++ 09 - variaveis e escopo/variaveis.js
@@ -0,0 +1,14 @@
+/*
+var var1 = 10;
+
+var var2 = '20';
+
+- O JavaScript apenas subtrai números então quando é outro tipo
+ele tenta converter o tipo da variável para fazer a operação
+
+console.log(var1 - var2); = - 10
+*/
+
+//O var só se restringe ao escopo global e de funções
+//A diferença entre var e let é que o let se restringe ao bloco em que está inserido (if, for, while, function, etc)
+//O const é igual ao let mas não permite reatribuições
---
+++ 10 - tipos de dados/tipos-dados.js
@@ -0,0 +1,12 @@
+/* 
+    boolean
+    null - ausência de valor
+    undefined - ausência de declaração
+    number
+    string
+    symbol
+*/
+
+// object
+
+console.log(!''); //negando a string ela vira um boolean
